b8e90e4f423976aab905c8832fc75c6e67f75aa3
Иван Дю <ivandu@air-ivan.dlink>
2017-06-18 15:15:55 +0700

    Initial Commit

---
+++ Istanbul_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Istanbul_app
+//
+//  Created by Иван Дю on 18.06.17.
+//  Copyright © 2017 Matthew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
