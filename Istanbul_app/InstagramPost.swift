3515a80e0880022593e0608e0464d2b7b16c4908
Иван Дю <ivandu@Air-Ivan.Dlink>
2017-06-18 16:40:26 +0700

    firstChanges

---
+++ Istanbul_app/InstagramPost.swift
@@ -0,0 +1,9 @@
+//
+//  InstagramPost.swift
+//  Istanbul_app
+//
+//  Created by Иван Дю on 18.06.17.
+//  Copyright © 2017 Matthew. All rights reserved.
+//
+
+import Foundation
---
+++ Istanbul_app/InstagramViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InstagramViewCell.swift
+//  Istanbul_app
+//
+//  Created by Иван Дю on 18.06.17.
+//  Copyright © 2017 Matthew. All rights reserved.
+//
+
+import UIKit
+
+class InstagramViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
